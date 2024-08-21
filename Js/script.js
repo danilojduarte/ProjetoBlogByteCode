3807e4de17dba7872e6fc4127e413272eab70f51
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-08-21 00:52:41 -0300

    Configurando function

--- Js/script.js
+++ Js/script.js
@@ -1,10 +1,12 @@
 // Filter Js
-$('.filter-item').click(function(){
-  const value = $(this).attr('data-flter')
-  if (value == 'all') {
-    $('post-box').show('1000')
-  }
-  else {
-    $('.post-box').not('.' + value).hilde('10000')
-  }
+$(document).ready(function(){
+  $('.filter-item').click(function(){
+    const value = $(this).attr('data-filter')
+    if(value == 'all') {
+      $('.post-box').show('1000')
+    }
+    else {
+      $('.post-box').not('.' + value).hide('')
+    }
+  })
 })
