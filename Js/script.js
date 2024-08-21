f096f8d11175ae72ef1cf67cd4ee7f1896cfb1c1
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-08-21 01:02:49 -0300

    Aplicando function header

--- Js/script.js
+++ Js/script.js
@@ -3,10 +3,21 @@ $(document).ready(function(){
   $('.filter-item').click(function(){
     const value = $(this).attr('data-filter')
     if(value == 'all') {
-      $('.post-box').show('1000')
+      $('.post-box').show('1000');
     }
     else {
-      $('.post-box').not('.' + value).hide('')
+      $('.post-box')
+      .not('.' + value)
+      .hide('1000');
+      $('.post-box')
+      .filter('.' + value)
+      .show('1000');
     }
-  })
-})
+  });
+  // Add btn active
+  $('.filter-item').click(function(){
+    $(this).addClass("active-filter").siblings().removeClass("active-filter");
+  });
+});
+
+// Header BackGround Change on scroll
